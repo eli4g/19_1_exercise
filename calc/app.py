289574747facda8a9ac3385935996b9e47c1ed23
eli4g <cgremont@gmail.com>
2022-03-22 20:47:48 -0400

    fixed issues with calc routes and fixed flask issue

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,52 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+
+app = Flask(__name__)
+
+
+@app.route("/add")
+def addition():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a,b)
+    return str(result)
+
+@app.route("/sub")
+def subtraction():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a,b)
+    return str(result)
+
+@app.route("/mult")
+def multiplication():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a,b)
+    return str(result)
+
+
+@app.route("/div")
+def division():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a,b)
+    return str(result)
+
+
+
+@app.route("/math/<calc_type>")
+def do_math(calc_type):
+    
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    
+    
+    do_math_type = {"add": add(a,b), "sub": sub(a,b), "mult": mult(a,b), "div": div(a,b)}
+    
+    
+    result = do_math_type[calc_type]
+    
+    return str(result)
--- greet/test.py
+++ greet/test.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+
 from app import app
 import unittest
 
